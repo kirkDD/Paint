57301a885b0b9be71f131eb70be5f59397e06d37
kirkD <kirkdai111@gmail.com>
2020-08-01 11:29:40 -0700

    suttle changes

--- app/src/main/java/painter/MainActivity.java
+++ app/src/main/java/painter/MainActivity.java
@@ -3,6 +3,8 @@ package painter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -12,6 +14,7 @@ import cse340.undo.R;
 public class MainActivity extends AppCompatActivity {
 
     /** View groups containing undo and redo menu buttons. */
+    ConstraintLayout mLayout;
     private ViewGroup mUndoMenu, mRedoMenu;
     Paper paper;
     @Override
@@ -22,12 +25,21 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) getSupportActionBar().hide();
 
         setContentView(R.layout.activity_full_screen);
+        mLayout = findViewById(R.id.layout);
         paper = findViewById(R.id.paper);
         paper.setBackgroundColor(Color.WHITE);
-
         findViewById(R.id.edit_action_button).setOnClickListener((v) -> {
             paper.editActionButtonClicked();
         });
+        // set up undo/redo button
+        mUndoMenu = (ViewGroup) getLayoutInflater().inflate(R.layout.undo_menu, mLayout , false);
+        mRedoMenu = (ViewGroup) getLayoutInflater().inflate(R.layout.redo_menu, mLayout , false);
+        mLayout.addView(mUndoMenu);
+        mLayout.addView(mRedoMenu);
+
+
+        findViewById(R.id.fab_undo).setOnClickListener((v) -> paper.undo());
+        findViewById(R.id.fab_redo).setOnClickListener((v) -> paper.redo());
     }
 
 
